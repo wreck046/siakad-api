69abb4f03f38042a135309fa35b2d999c33d39b8
wreck046 <ivankoroh46@gmail.com>
2026-03-23 18:23:02 +0800

    apply a dummy token

---
+++ src/main/java/siakad_api/controller/AuthController.java
@@ -0,0 +1,28 @@
+package siakad_api.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/auth/")
+@CrossOrigin(origins = "http://localhost:5173")
+public class AuthController {
+
+   @PostMapping("/login")
+    public Map<String, Object> login(@RequestBody Map<String, String> request) {
+        String username = request.get("username");
+        String password = request.get("password");
+
+        if ("admin".equals(username) && "123".equals(password)) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("token", "dummy-token-123");
+            response.put("username", username);
+            return response;
+        } else {
+            throw new RuntimeException("Username atau password salah");
+        }
+    }
+}
