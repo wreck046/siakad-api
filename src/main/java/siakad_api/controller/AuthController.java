21130817d838d98b4aada22e039289b36f7eaf0c
wreck046 <ivankoroh46@gmail.com>
2026-03-27 10:31:44 +0800

    fixing crossorigin of controllers

--- src/main/java/siakad_api/controller/AuthController.java
+++ src/main/java/siakad_api/controller/AuthController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/auth/")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
    @PostMapping("/login")
--- src/main/java/siakad_api/controller/SiswaController.java
+++ src/main/java/siakad_api/controller/SiswaController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/siswa")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class SiswaController {
     
     private final SiswaService service;
