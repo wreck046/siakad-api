c1aee7f9013c76e644d807fae99c0c0e21751504
wreck046 <ivankoroh46@gmail.com>
2026-03-29 19:34:23 +0800

    expiring token shorten to 1 hour max

--- src/main/java/siakad_api/utils/JwtUtil.java
+++ src/main/java/siakad_api/utils/JwtUtil.java
@@ -4,6 +4,8 @@ import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.util.Date;
 
+import javax.management.RuntimeErrorException;
+
 public class JwtUtil {
 
     private static final String SECRET = "secretkeysecretkeysecretkey123456";
@@ -13,17 +15,22 @@ public class JwtUtil {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000))  //token expired setelah 1 jam
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public static String validateToken(String token) {
-        return Jwts.parserBuilder()
+        try {
+            return Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
+        } catch (ExpiredJwtException e) {
+            throw new RuntimeException("Token expired");
+        }
+        
     }
 }
